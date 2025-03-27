de886420f6073fecb9c5581fdc528530cbc58a18
Brian Achaye <acheonen@gmail.com>
2025-03-27 21:08:50 +0300

    updated

--- project_sql/2_top_paying_job_skills.sql
+++ project_sql/2_top_paying_job_skills.sql
@@ -40,30 +40,23 @@ ORDER BY
 /*
 Key Insights from the Skills:
 
-    Top 3 Most In-Demand Skills:
-        - SQL (8 mentions) – The most frequently required skill, emphasizing its importance in querying and managing data.
-        - Python (7 mentions) – A key programming language for data analysis, automation, and machine learning.
-        - Tableau (6 mentions) – A leading data visualization tool, highlighting the need for dashboard and reporting skills.
+Top 3 Most In-Demand Skills:
+    - SQL (8 mentions) – The most frequently required skill, emphasizing its importance in querying and managing data.
+    - Python (7 mentions) – A key programming language for data analysis, automation, and machine learning.
+    - Tableau (6 mentions) – A leading data visualization tool, highlighting the need for dashboard and reporting skills.
 
-    Other Notable Skills:
+Other Notable Skills:
+    - R (4 mentions) – Still relevant for statistical analysis and data science.
+    - Snowflake, Pandas, Excel (3 mentions each) – Snowflake for cloud-based data warehousing, Pandas for data manipulation, and Excel for traditional data analysis.
 
-        R (4 mentions) – Still relevant for statistical analysis and data science.
+Cloud & Big Data Skills:
+    - Azure, AWS (2 mentions each) – Cloud computing platforms are increasingly relevant for data analysts.
+    - Hadoop, Databricks, Pyspark (1 mention each) – Skills in big data processing frameworks are in demand but less frequent in these job postings.
 
-        Snowflake, Pandas, Excel (3 mentions each) – Snowflake for cloud-based data warehousing, Pandas for data manipulation, and Excel for traditional data analysis.
+Version Control & Collaboration Tools:
+    - Bitbucket, GitLab, Atlassian, Jira, Confluence (2 mentions each) – These tools are important for managing code and projects.
 
-    Cloud & Big Data Skills:
-
-        Azure, AWS (2 mentions each) – Cloud computing platforms are increasingly relevant for data analysts.
-
-        Hadoop, Databricks, Pyspark (1 mention each) – Skills in big data processing frameworks are in demand but less frequent in these job postings.
-
-    Version Control & Collaboration Tools:
-
-        Bitbucket, GitLab, Atlassian, Jira, Confluence (2 mentions each) – These tools are important for managing code and projects.
-
-    Less Common but Valuable Skills:
-
-        Power BI (2 mentions) – Another strong business intelligence tool.
-
-        Jupyter, Numpy (2 mentions each) – Popular for data science workflows.
+Less Common but Valuable Skills:
+    - Power BI (2 mentions) – Another strong business intelligence tool.
+    - Jupyter, Numpy (2 mentions each) – Popular for data science workflows.
 */
